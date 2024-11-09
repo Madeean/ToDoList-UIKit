d2ee0d8c6984e3c9be80b472782d34e9d81ded44
MadeReihan <reihanmade@gmail.com>
2024-11-09 22:01:13 +0700

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by made reihan on 09/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
