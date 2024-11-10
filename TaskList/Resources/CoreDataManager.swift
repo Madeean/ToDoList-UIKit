2122b66611ed3efa5559446aa5ad8f70f0f2071a
MadeReihan <reihanmade@gmail.com>
2024-11-10 20:37:01 +0700

    CRD core data

--- TaskList/Resources/CoreDataManager.swift
+++ TaskList/Resources/CoreDataManager.swift
@@ -22,14 +22,14 @@ class CoreDataManager {
         }
         return container
     }()
-    
+
     var context: NSManagedObjectContext {
         persistentContainer.viewContext
     }
-    
+
     func saveContext() {
         if context.hasChanges {
-            do{
+            do {
                 try context.save()
             } catch {
                 let nsError = error as NSError
@@ -37,4 +37,77 @@ class CoreDataManager {
             }
         }
     }
+
+    func getAll() -> [Task] {
+        var tasks = [Task]()
+
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let sortByDueDate = NSSortDescriptor(key: "dueOn", ascending: true)
+        fetchRequest.sortDescriptors = [sortByDueDate]
+
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+
+        return tasks
+    }
+
+    func addNewTask(name: String, dueOn: Date) {
+        let task = Task(context: context)
+        task.name = name
+        task.dueOn = dueOn
+        task.id = UUID()
+        task.completed = false
+        task.completeOn = dueOn.advanced(by: 100000)
+
+        saveContext()
+    }
+
+    func toggleCompleted(id: UUID) {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let predicate = NSPredicate(format: "id=%@", id.uuidString)
+        fetchRequest.predicate = predicate
+
+        do {
+//            let fetchedTasks = try context.fetch(fetchRequest)
+//            for task in fetchedTasks {
+//                task.completed = !task.completed
+//                if task.completed {
+//                    task.completeOn = Date()
+//                }
+//            }
+
+            if let fetchedTask = try context.fetch(fetchRequest).first(where: { $0.id == id }) {
+                fetchedTask.completed = !fetchedTask.completed
+                if fetchedTask.completed {
+                    fetchedTask.completeOn = Date()
+                }
+            }
+            
+            saveContext()
+
+        } catch let error as NSError {
+            print("Error togglein state: \(error.userInfo), \(error.localizedDescription)")
+        }
+    }
+    
+    func delete(id: UUID) {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        fetchRequest.predicate = NSPredicate(format: "id=%@", id.uuidString)
+        
+        do {
+            let fetchedTasks = try context.fetch(fetchRequest)
+            for task in fetchedTasks {
+                context.delete(task)
+            }
+            
+            saveContext()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
 }
+ 
--- TaskList/ViewControllers/ToDoListViewController.swift
+++ TaskList/ViewControllers/ToDoListViewController.swift
@@ -6,11 +6,119 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemBackground
         title = "Madee"
     }
 }
+
+//class ToDoListViewController: UIViewController {
+//    lazy var addNewButton: UIButton = {
+//        let v = UIButton()
+//        v.translatesAutoresizingMaskIntoConstraints = false
+//        v.setTitle("Add Task", for: .normal)
+//        v.setTitleColor(.black, for: .normal)
+//        v.addTarget(self, action: #selector(addNewTask(sender:)), for: .touchUpInside)
+//        return v
+//    }()
+//    
+//    lazy var getTasksButton: UIButton = {
+//        let v = UIButton()
+//        v.translatesAutoresizingMaskIntoConstraints = false
+//        v.setTitle("Get Task", for: .normal)
+//        v.setTitleColor(.black, for: .normal)
+//        v.addTarget(self, action: #selector(getTasks(sender:)), for: .touchUpInside)
+//        return v
+//    }()
+//    
+//    lazy var toggleCompleteButton: UIButton = {
+//        let v = UIButton()
+//        v.translatesAutoresizingMaskIntoConstraints = false
+//        v.setTitle("Mark Task Complete", for: .normal)
+//        v.setTitleColor(.black, for: .normal)
+//        v.addTarget(self, action: #selector(markCompleted(sender:)), for: .touchUpInside)
+//        return v
+//    }()
+//    
+//    lazy var deleteTaskButton: UIButton = {
+//        let v = UIButton()
+//        v.translatesAutoresizingMaskIntoConstraints = false
+//        v.setTitle("delete task", for: .normal)
+//        v.setTitleColor(.black, for: .normal)
+//        v.addTarget(self, action: #selector(deleteTask(sender:)), for: .touchUpInside)
+//        return v
+//    }()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = UIColor.systemBackground
+//        title = "Madee"
+//        
+//        [addNewButton, getTasksButton, toggleCompleteButton, deleteTaskButton].forEach {control in
+//            view.addSubview(control)
+//        }
+//        
+//        NSLayoutConstraint.activate([
+//            addNewButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            addNewButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+//            addNewButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+//            
+//            getTasksButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            getTasksButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+//            getTasksButton.topAnchor.constraint(equalTo: addNewButton.bottomAnchor, constant: 8),
+//            
+//            toggleCompleteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            toggleCompleteButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+//            toggleCompleteButton.topAnchor.constraint(equalTo: getTasksButton.bottomAnchor, constant: 8),
+//            
+//            deleteTaskButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            deleteTaskButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+//            deleteTaskButton.topAnchor.constraint(equalTo: toggleCompleteButton.bottomAnchor, constant: 8),
+//        ])
+//    }
+//    
+//    @objc
+//    func addNewTask(sender: UIButton) {
+//        CoreDataManager.shared.addNewTask(name: "new task", dueOn: Date().addingTimeInterval(100000))
+//    }
+//    
+//    @objc
+//    func getTasks(sender: UIButton) {
+//        let tasks = CoreDataManager.shared.getAll()
+//        for task in tasks {
+//            print(task.name ?? "kosong")
+//        }
+//    }
+//    
+//    @objc
+//    func markCompleted(sender: UIButton) {
+//        let tasks = CoreDataManager.shared.getAll()
+//        for task in tasks {
+//            CoreDataManager.shared.toggleCompleted(id: task.id ?? UUID())
+//        }
+//        
+//        let fetchedTask = CoreDataManager.shared.getAll()
+//        for task in fetchedTask {
+//            print("\(task.name ?? "") : \(task.completed), \(task.completeOn?.formatted(date: .abbreviated, time: .omitted) ?? "") ")
+//        }
+//    }
+//    
+//    @objc
+//    func deleteTask(sender: UIButton) {
+//        let tasks = CoreDataManager.shared.getAll()
+//        for task in tasks {
+//            CoreDataManager.shared.delete(id: task.id ?? UUID())
+//        }
+//        
+//        let fetchedTask = CoreDataManager.shared.getAll()
+//        print(fetchedTask.count)
+//        for task in fetchedTask {
+//            print("\(task.name ?? "") : \(task.completed), \(task.completeOn?.formatted(date: .abbreviated, time: .omitted) ?? "") ")
+//        }
+//    }
+//}
